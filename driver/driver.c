dc65193bd263a6fb969e6d2f170354a39d8b25d6
RCQQYY <adsl53102@gmail.com>
2022-12-09 01:00:17 +0800

    Modify code

--- driver/driver.c
+++ driver/driver.c
@@ -2,35 +2,36 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/string.h>
+#include <linux/cdev.h>
 
 MODULE_DESCRIPTION("hello_world");
 MODULE_LICENSE("GPL");
 
-static dev_t mydev = MKDEV(202,128);
+static dev_t my_dev = MKDEV(202,128);
 static struct cdev my_cdev;
 static int BUF_SIZE = 1024;
-static char BUF[BUF_SIZE] = "";
+static char BUF[1024] = "";
 
-ssize_t my_read(struct file*, char*, size_t, loff_t*);
-ssize_t my_write(struct file*, const char*, size_t, loff_t*);
-int open(struct inode *, struct file *);
-int my_release(struct inode*, struct file*);
+static ssize_t my_read(struct file*, char*, size_t, loff_t*);
+static ssize_t my_write(struct file*, const char*, size_t, loff_t*);
+static int my_open(struct inode *, struct file *);
+static int my_release(struct inode*, struct file*);
 
-static struct file_oeprations my_operations = {
+static struct file_operations my_operations = {
     .owner = THIS_MODULE,
     .open = my_open,
     .read = my_read,
     .write = my_write,
     .release = my_release
-}
+};
 
 
 
-ssize_t my_read(struct file* , char*, size_t, loff_t*)
+static ssize_t my_read(struct file* , char __user* buf, size_t, loff_t*)
 {
     size_t length = strlen(BUF);
     
-    if (copy_to_user(BUF, length))
+    if (copy_to_user(buf, BUF, length))
     {
         printk(KERN_INFO "copy to user failed\n");
         return -EFAULT;
@@ -39,11 +40,11 @@ ssize_t my_read(struct file* , char*, size_t, loff_t*)
     return length;
 }
 
-ssize_t my_write(struct file* filp, const char __user* buf, size_t count, loff_t* pos)
+static ssize_t my_write(struct file* filp, const char __user* buf, size_t count, loff_t* pos)
 {
     size_t length = strnlen_user(BUF, BUF_SIZE);
 
-    if (copy_from_user(BUF, length))
+    if (copy_from_user(BUF, buf, length))
     {
         printk(KERN_INFO "copy from user failed\n");
         return -EFAULT;
@@ -52,14 +53,18 @@ ssize_t my_write(struct file* filp, const char __user* buf, size_t count, loff_t
     return length;
 }
 
-int my_open(struct inode* , struct file *)
+static int my_open(struct inode* , struct file *)
 {
     printk(KERN_INFO "open\n");
+
+    return 0;
 }
 
-int my_release(struct inode*, struct file*)
+static int my_release(struct inode*, struct file*)
 {
     printk(KERN_INFO "release\n");
+    
+    return 0;
 }
 
 static int __init hello_init(void)
---
+++ driver/user_program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int bufsize = 1024;
+    char buf[bufsize];
+
+    int fd = open("/dev/driver", O_RDWR);
+
+    int ret = write(fd, buf, bufsize);
+    
+    ret = read(fd, buf, bufsize);
+
+    close(fd);
+
+    return 0;
+}
+
